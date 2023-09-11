9c83175d1719ef1706c3ba8fdf5dde16f4056a4b
shankar <shankar.m@worktual.com>
2023-09-11 14:30:04 +0530

    external files removed

--- jwtConsole.js
+++ jwtConsole.js
@@ -1,10 +1,26 @@
-import docusign from "docusign-esign";
-import { sendEnvelope } from "./signingViaEmail.js";
 import fs from "fs";
 import path from "path";
-import jwtConfig from "./jwtConfig.json" assert { type: "json" };
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import pkg from "docusign-esign";
+const {
+  ApiClient,
+  EnvelopesApi,
+  SignHere,
+  Document,
+  EnvelopeDefinition,
+  Signer,
+  CarbonCopy,
+  Tabs,
+  Recipients,
+} = pkg;
+
+const jwtConfig = {
+  "dsJWTClientId": "2c85fe53-6708-42b3-b916-b8e4f1862855",
+  "impersonatedUserGuid": "cd538e3a-54c5-472d-a4a6-7e955ea6d7ab",
+  "privateKeyLocation":  "./private.key",
+  "dsOauthServer": "https://account-d.docusign.com"
+};
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -38,7 +54,7 @@ function getConsent() {
 
 async function authenticate() {
   const jwtLifeSec = 10 * 60, // requested lifetime for the JWT is 10 min
-    dsApi = new docusign.ApiClient();
+    dsApi = new ApiClient();
   dsApi.setOAuthBasePath(jwtConfig.dsOauthServer.replace("https://", "")); // it should be domain only.
   let rsaKey = fs.readFileSync(jwtConfig.privateKeyLocation);
 
@@ -86,7 +102,6 @@ async function authenticate() {
 }
 
 function getArgs(apiAccountId, accessToken, basePath) {
-
   const envelopeArgs = {
     signerEmail: "marineshankar@gmail.com",
     signerName: "Shankar",
@@ -106,15 +121,97 @@ function getArgs(apiAccountId, accessToken, basePath) {
   return args;
 }
 
+export const sendEnvelope = async (args) => {
+  let dsApiClient = new ApiClient();
+  dsApiClient.setBasePath(args.basePath);
+  dsApiClient.addDefaultHeader("Authorization", "Bearer " + args.accessToken);
+  let envelopesApi = new EnvelopesApi(dsApiClient);
+  let results = null;
+
+  let envelope = makeEnvelope(args.envelopeArgs);
+
+  results = await envelopesApi.createEnvelope(args.accountId, {
+    envelopeDefinition: envelope,
+  });
+  let envelopeId = results.envelopeId;
+
+  console.log(`Envelope was created. EnvelopeId ${envelopeId}`);
+  return { envelopeId: envelopeId };
+};
+
+function makeEnvelope(args) {
+  let doc3PdfBytes;
+
+  doc3PdfBytes = fs.readFileSync(args.doc3File);
+
+  let env = new EnvelopeDefinition();
+  env.emailSubject = "Exhibit B Terms & Conditions - Document by Worktual";
+
+  let doc = new Document.constructFromObject({
+    documentBase64: Buffer.from(doc3PdfBytes).toString("base64"),
+    name: "Exhibit B Terms & Conditions",
+    fileExtension: "pdf",
+    documentId: "1",
+  });
+
+  env.documents = [doc];
+
+  let signer1 = Signer.constructFromObject({
+    email: args.signerEmail,
+    name: args.signerName,
+    recipientId: "1",
+    routingOrder: "1",
+  });
+
+  let cc1 = new CarbonCopy();
+  cc1.email = args.ccEmail;
+  cc1.name = args.ccName;
+  cc1.routingOrder = "2";
+  cc1.recipientId = "2";
+
+  let signHere1 = SignHere.constructFromObject({
+    anchorString: "Signature:",
+    anchorYOffset: "5",
+    anchorUnits: "pixels",
+    anchorXOffset: "20",
+  });
+
+  let signer1Tabs = Tabs.constructFromObject({
+    signHereTabs: [signHere1],
+  });
+  signer1.tabs = signer1Tabs;
+
+  let recipients = Recipients.constructFromObject({
+    signers: [signer1],
+    carbonCopies: [cc1],
+  });
+  env.recipients = recipients;
+
+  env.status = args.status;
+
+  return env;
+}
+
 async function main() {
   let accountInfo = await authenticate();
-  let args = getArgs(
-    accountInfo.apiAccountId,
-    accountInfo.accessToken,
-    accountInfo.basePath
-  );
+
+  const envelopeArgs = {
+    signerEmail: "marineshankar@gmail.com",
+    signerName: "Shankar",
+    ccEmail: "networkingshankar@gmail.com",
+    ccName: "NetworkingShankar",
+    status: "sent",
+    doc3File: path.resolve(demoDocsPath, doc3File),
+  };
+
+  const args = {
+    accessToken: accountInfo.accessToken,
+    basePath: accountInfo.basePath,
+    accountId: accountInfo.apiAccountId,
+    envelopeArgs: envelopeArgs,
+  };
+
   let envelopeId = sendEnvelope(args);
-  console.log(envelopeId);
 }
 
 main();
